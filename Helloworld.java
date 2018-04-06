8d11058fb06a20585a65f94772da62302c178c37
sudhadesigan <38094937+sudhadesigan@users.noreply.github.com>
2018-04-06 12:07:23 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+
+public class Helloworld {
+	
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		String m = s.next();
+		int n= s.nextInt();
+	Double d = s.nextDouble();
+	s.close();
+	
+	
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(d);
+		
+	System.out.println("Hello World");
+	System.out.print("Hello Java");
+
+}
+}
